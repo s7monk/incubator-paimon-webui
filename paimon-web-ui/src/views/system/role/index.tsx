2729effa59d9bc66c817359408fb6e14d80b33b0
s7monk <15512826113@163.com>
2024-07-10 18:11:35 +0800

    commit.

--- paimon-web-ui/src/views/system/role/index.tsx
+++ paimon-web-ui/src/views/system/role/index.tsx
@@ -27,7 +27,7 @@ import { createRole, getPermissionByRoleId, updateRole } from '@/api/models/role
 import type { Role, RoleDTO, RoleMenu } from '@/api/models/role/types'
 
 export default defineComponent({
-  name: 'RolePage',
+  name: 'RolePage2',
   setup() {
     const { t } = useLocaleHooks()
     const { tableVariables, getTableData, roleList, loading } = useTable()
